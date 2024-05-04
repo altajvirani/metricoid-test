323134b1cabf6def08e87b9145baeb125e29126f
altajvirani <altajvirani786@gmail.com>
2024-05-04 17:59:38 +0530

    test files

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,63 +1,241 @@
 /* eslint-disable no-unused-vars */
 import "bootstrap/dist/css/bootstrap.min.css";
-import Table from "react-bootstrap/Table";
-import Button from "react-bootstrap/Button";
-import { useState, useEffect } from "react";
+import { Dropdown, Table, Button, Form, FormControl } from "react-bootstrap";
+import { useState, useEffect, useRef } from "react";
 import axios from "axios";
 
 function App() {
+  const [products, setProducts] = useState([]);
   const [orders, setOrders] = useState([]);
+
   const [newOrder, setNewOrder] = useState({
     c_name: "nisha",
     p_name: "speaker",
     quantity: 10,
-    price: 100000,
+    o_price: 100000,
   });
+
+  const customerName = useRef(null);
+  const [selectedProduct, setSelectedProduct] = useState();
+  const [quantity, setQuantity] = useState(1);
+  const [totalPrice, setTotalPrice] = useState(0);
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   useEffect(() => {
     const getOrders = async () => {
-      const fetchOrders = await axios.get("http://localhost:5000/get-orders");
-
-      setOrders(fetchOrders.data.orders);
+      await axios
+        .get("http://localhost:5000/get-orders")
+        .then((fetchedOrders) => setOrders(fetchedOrders.data.orders))
+        .catch((err) => {
+          console.error(`Error fetching orders: ${err}`);
+        });
     };
+
     getOrders();
+
+    const getProducts = async () => {
+      await axios
+        .get("http://localhost:5000/get-products")
+        .then((fetchedProducts) => setProducts(fetchedProducts.data.products))
+        .catch((err) => {
+          console.error(`Error fetching products: ${err}`);
+        });
+    };
+
+    getProducts();
   }, []);
 
   useEffect(() => {
-    console.log(orders);
-  }),
-    [orders];
+    console.log(products);
+  }, [products]);
 
-  const handleOrder = async () => {
-    await axios.post("http://localhost:5000/new-order/", newOrder, {});
+  const handleNewOrder = async () => {
+    await axios.post("http://localhost:5000/new-order/", newOrder);
   };
 
   return (
     <>
-      <Table>
-        <thead>
-          <tr>
-            <td>ID</td>
-            <td>Customer</td>
-            <td>Product</td>
-            <td>Quantity</td>
-            <td>Total Price</td>
-          </tr>
-        </thead>
-        <tbody>
-          {orders.map((index, order) => {
-            <tr key={index}>
-              <td>{index + 1}</td>
-              <td>{order.c_name}</td>
-              <td>{order.p_name}</td>
-              <td>{order.quantity}</td>
-              <td>{order.price}</td>
-            </tr>;
-          })}
-        </tbody>
-      </Table>
+      <div
+        style={{
+          width: "100dvw",
+          height: "100dvh",
+        }}>
+        <Table>
+          <thead>
+            <tr>
+              <td>ID</td>
+              <td>Customer</td>
+              <td>Product</td>
+              <td>Quantity</td>
+              <td>Total Price</td>
+            </tr>
+          </thead>
+          <tbody>
+            {orders.map((order, index) => {
+              return (
+                <tr key={index}>
+                  <td>{index + 1}</td>
+                  <td>{order.c_name}</td>
+                  <td>{order.p_name}</td>
+                  <td>{order.quantity}</td>
+                  <td>{order.o_price}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </Table>
 
-      <Button onClick={() => handleOrder()}>+</Button>
+        <Button
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            position: "absolute",
+            bottom: "1rem",
+            right: "1rem",
+            borderRadius: "0.6rem",
+            padding: "0.5rem 0.8rem",
+          }}
+          onClick={() => setIsModalOpen((prev) => !prev)}>
+          +
+        </Button>
+      </div>
+
+      {isModalOpen && (
+        <Form.Group
+          controlId="formNewOrder"
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            gap: "1rem",
+            position: "fixed",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+            border: "0.01rem solid #bfbfbf",
+            borderRadius: "0.4rem",
+            backgroundColor: "white",
+            width: "20rem",
+            height: "max-content",
+            padding: "1rem 0 1rem 0",
+          }}>
+          <div
+            style={{
+              width: "100%",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "space-between",
+              padding: "0 1rem 0.5rem 1rem",
+              borderBottom: "0.01rem solid #bfbfbf",
+            }}>
+            <span>New Order</span>
+            <span
+              style={{
+                cursor: "pointer",
+                backgroundColor: "#bfbfbf",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                borderRadius: "0.4rem",
+                padding: "0.25rem 0.65rem",
+              }}
+              onClick={() => setIsModalOpen(false)}>
+              X
+            </span>
+          </div>
+          <div
+            style={{
+              padding: "0 1rem",
+            }}>
+            <Form.Label>Customer Name</Form.Label>
+            <FormControl type="text" placeholder="Eg. John Doe" />
+          </div>
+          <div
+            style={{
+              padding: "0 1rem",
+            }}>
+            <Form.Select
+              tyle={{ width: "100%" }}
+              onChange={(e) => {
+                setSelectedProduct(e.target.value);
+
+                setTotalPrice(() => {
+                  const selectedProductPrice = products.find(
+                    (product) =>
+                      selectedProduct &&
+                      selectedProduct.toLowerCase() ===
+                        product.p_name.toLowerCase()
+                  );
+                  return selectedProductPrice
+                    ? selectedProductPrice.p_price * quantity
+                    : 0;
+                });
+              }}>
+              <option selected="true" disabled="true">
+                Product Name
+              </option>
+              {products.map((product, index) => {
+                return (
+                  <option key={index}>
+                    {product.p_name.charAt(0).toUpperCase() +
+                      product.p_name.slice(1)}
+                  </option>
+                );
+              })}
+            </Form.Select>
+          </div>
+
+          <div
+            style={{
+              padding: "0 1rem",
+            }}>
+            <Form.Label>Quantity</Form.Label>
+            <FormControl
+              type="number"
+              min={1}
+              placeholder="Eg. 1"
+              defaultValue={1}
+              onChange={(e) => {
+                setQuantity(e.target.value);
+              }}
+            />
+          </div>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              alignItems: "center",
+              justifyContent: "space-between",
+              width: "100%",
+              padding: "0 1rem",
+            }}>
+            <span>Total Price</span>
+            <span>{0}</span>
+          </div>
+
+          <div
+            style={{
+              width: "100%",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "end",
+              padding: "0 1rem",
+            }}>
+            <Button
+              style={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                borderRadius: "0.6rem",
+                padding: "0.5rem 0.8rem",
+              }}
+              onClick={handleNewOrder}>
+              Add Order
+            </Button>
+          </div>
+        </Form.Group>
+      )}
     </>
   );
 }
--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,17 +1,51 @@
 const Order = require("../models/Order");
+const Product = require("../models/Product");
 
 const router = require("express").Router();
 
-router.get("/get-orders", async (_, res) => {
-  const orders = await Order.find();
+router.get("/get-products", async (_, res) => {
+  try {
+    const products = await Product.find();
 
-  console.log(orders);
-
-  res.json({
-    orders,
-  });
+    if (!products) res.status(404).json({ message: "No products found." });
+    else res.json({ products });
+  } catch (err) {
+    res.status(500).json({
+      message: `Error while fetching products: ${err}`,
+    });
+  }
 });
 
-router.post("/new-order", async (req, res) => {});
+router.get("/get-orders", async (_, res) => {
+  try {
+    const orders = await Order.find();
+
+    if (!orders) res.status(404).json({ message: "No orders found." });
+    else res.json({ orders });
+  } catch (err) {
+    res.status(500).json({
+      message: `Error while fetching orders: ${err}`,
+    });
+  }
+});
+
+router.post("/new-order", async (req, res) => {
+  try {
+    const product = req.body.hasOwnProperty("p_name") ? req.body.p_name : null;
+
+    const isValidProduct = await Product.findOne({ p_name: product });
+
+    if (!isValidProduct)
+      res.status(400).json({ message: "Please enter a valid product name." });
+    else {
+      await new Order(req.body).save();
+      res.json({ message: "Successfully added new order." });
+    }
+  } catch (err) {
+    res.status(500).json({
+      message: `Error while adding order: ${err}`,
+    });
+  }
+});
 
 module.exports = router;
