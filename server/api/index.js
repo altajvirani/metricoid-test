1b445328968d8574a57114fb79c967d74158c81b
altajvirani <altajvirani786@gmail.com>
2024-05-05 15:15:53 +0530

    test files

--- server/api/index.js
+++ server/api/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 
 require("dotenv").config();
 
+const PORT = process.env.PORT || 5000;
+
 const app = express();
 
 const dbConn = require("./db");
@@ -10,13 +12,14 @@ dbConn(process.env.DB_URL);
 
 const cors = require("cors");
 
-app.use(
-  cors({
-    origin: "*",
-    methods: "*",
-    allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true,
-  })
-);
+app.use(cors());
 
 app.use(express.json());
+
+const routes = require("../routes/routes");
+
+app.use(routes);
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}.`);
+});
